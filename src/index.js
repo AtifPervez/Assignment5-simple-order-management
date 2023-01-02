2e0fae7a0399716d9db1f8aac5ed2f0165709ff4
Atif Pervez <atifpervez0@gmail.com>
2023-01-02 16:25:18 +0530

    second

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const router = require("./router/route")
+const mongoose = require('mongoose')
+const bodyParser = require("body-parser")
+const app = express()
+app.use(bodyParser.json())
+mongoose.connect("mongodb+srv://atifpervez:34BmDa5XVvtznQvO@code.8mvlc.mongodb.net/assignmet5_simpleOrderManagement")
+    .then(() => {
+        console.log("Connected With MongoDb");
+    })
+    .catch(err => {
+        console.log(err.message);
+    })
+app.use("/", router)
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Express App Is Running On Port " + (process.env.PORT || 3000));
+})
